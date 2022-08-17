18ee7e7fe2702bc70c292e50bd573c2d695fcc4b
Silvio Dias <silvio.dias@etec.sp.gov.br>
2022-08-17 02:19:11 -0300

    refactor: changing TaskCard component

--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -1,40 +1,49 @@
 import React, { useState } from 'react'
 
-import { FiTrash2 } from 'react-icons/fi'
+import { ITaskDTO } from '@dtos/taskDTO'
 
 import { Checkbox } from '@components/Checkbox'
+import { TaskDeleteDialog } from '@components/TaskDeleteDialog'
 
 import styles from './styles.module.css'
 
 interface Props {
   index: number
+  data: ITaskDTO
+  onCheck(value: ITaskDTO): void
+  onDelete(value: ITaskDTO): void
 }
 
-export const TaskCard: React.FC<Props> = ({ index }) => {
+export const TaskCard: React.FC<Props> = ({
+  index,
+  data,
+  onCheck,
+  onDelete
+}) => {
   const [isCheck, setCheck] = useState<boolean>(false)
 
   function handleTaskCheck() {
     setCheck(!isCheck)
+    onCheck(data)
+  }
+
+  function handleTaskDelete() {
+    onDelete(data)
   }
 
   return (
     <div
       className={`${styles.container} ${isCheck && styles.checked}`}
-      style={{ animationDuration: `${1 + index / 10}s` }}
+      style={{ animationDuration: `${0.8 + index / 12}s` }}
     >
       <div className={styles.content}>
         <div className={styles.checkbox}>
           <Checkbox checked={isCheck} onClick={handleTaskCheck} />
         </div>
-        <p>
-          teger urna interdum massa libero auctor neque turpis turpis semper.
-          Duis vel sed fames integer.
-        </p>
+        <p>{data.value}</p>
       </div>
 
-      <button>
-        <FiTrash2 size={16} />
-      </button>
+      <TaskDeleteDialog onConfirm={handleTaskDelete} />
     </div>
   )
 }
