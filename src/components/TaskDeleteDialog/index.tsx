98a493c884a65485e19c603180fe1e1d4ee43b3c
SilvioDiasJr <silviodiasjr@outlook.com>
2022-08-19 19:53:58 -0300

    refactor: changing TaskDeleteDialog component

--- src/components/TaskDeleteDialog/index.tsx
+++ src/components/TaskDeleteDialog/index.tsx
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import * as Dialog from '@radix-ui/react-dialog'
 import { FiTrash2, FiX } from 'react-icons/fi'
+import { deleteDoc, doc } from 'firebase/firestore/lite'
+
+import { db } from '@services/firebaseConnection'
 
 import styles from './styles.module.css'
+import { Loading } from '@components/Loading'
 
 interface Props {
   onConfirm(): void
+  taskID: string
 }
 
-export const TaskDeleteDialog: React.FC<Props> = ({ onConfirm }) => {
-  function handleConfirm() {
-    onConfirm()
+export const TaskDeleteDialog: React.FC<Props> = ({ onConfirm, taskID }) => {
+  const [isLoading, setLoading] = useState<boolean>(false)
+  const [open, setOpen] = useState<boolean>(false)
+
+  async function handleDeleteTask() {
+    try {
+      setLoading(true)
+      await deleteDoc(doc(db, 'tasks', taskID))
+
+      onConfirm()
+      setOpen(false)
+    } catch (error: any) {
+      console.log(error.response.data.message)
+    } finally {
+      setLoading(false)
+    }
   }
+
   return (
-    <Dialog.Root>
+    <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Trigger className={styles.deleteButton}>
         <FiTrash2 size={16} />
       </Dialog.Trigger>
 
-      <Dialog.Portal>
+      <Dialog.Portal style={{ position: 'relative' }}>
         <Dialog.Overlay className={styles.overlay} />
 
         <Dialog.Content className={styles.content}>
@@ -36,7 +55,10 @@ export const TaskDeleteDialog: React.FC<Props> = ({ onConfirm }) => {
           <div className={styles.footer}>
             <Dialog.Close>Cancelar</Dialog.Close>
             <div className={styles.separator}></div>
-            <button onClick={handleConfirm}>Apagar</button>
+
+            <button onClick={handleDeleteTask}>
+              {isLoading ? <Loading /> : 'Apagar'}
+            </button>
           </div>
         </Dialog.Content>
       </Dialog.Portal>
